5490bb78a7fb465927c4cf00baee031b3123c2c7
BMan-21 <bodhibhatia@gmail.com>
2026-04-29 20:16:59 -0700

    added anim

--- Scripts/dive_bomber.gd
+++ Scripts/dive_bomber.gd
@@ -6,11 +6,16 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	velocity.y = delta * speed
-	
+	if global_position.y > -250:
+		pass
+		#play die anim
 	altitude_arrow.text = str(round(global_position.y * -100)/10)
 	if Input.is_action_pressed("bomb drop"):
 		print("drop")
-		if global_position.y < 350 and global_position.y > 250:
-			pass
+		if global_position.y > -350 and global_position.y < -250:
+			get_tree().change_scene_to_file("res://scenes/bomb successful.tscn")
+		elif global_position.y < -350:
+			get_tree().change_scene_to_file("res://scenes/bomb missed.tscn")
+			#play miss anim
 
 	move_and_slide()
